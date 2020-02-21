8106779a12b31b5aae6affb6bc9b4cc55a60e716
Florent PASTOR <flopastor06@gmail.com>
2020-02-21 11:43:20 +0100

    change blog design

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import os
 import markdown2
 import urllib.request
 import xmltodict
+import json
 from flask import Flask, render_template, make_response
 
-from core.entities import Picture
+from core.entities import Picture, Blog
 from core.utils import get_file_lines
 
 app = Flask(__name__)
 PORT = 5000
-# RSS_DIRECTORY = "rss_items"
 FILE_SKILLS = "skills.txt"
 
 
@@ -17,30 +17,35 @@ def get_skills():
     return ''.join(get_file_lines(FILE_SKILLS)).split('\n')
 
 
-# def get_blogs():
-#     blog_list = [Blog(RSS_DIRECTORY + "/" + f) for f in os.listdir(RSS_DIRECTORY) if f.endswith(".md")]
-#     return [b for b in blog_list if b.is_valid()]
+def get_blogs():
+    url = "https://raw.githubusercontent.com/earendil06/blog/master/publish.json"
+    stream = urllib.request.urlopen(url)
+    data = stream.read()
+    stream.close()
+    items = json.loads(data)
+    blog_list = [Blog(b['title'], b['date'], b['filename']) for b in items]
+    return blog_list
 
 
 @app.route('/')
 def home():
-    return render_template('home.html', skills=get_skills())
+    return render_template('home.html', skills=get_skills(), blogs=get_blogs())
 
 
-# @app.route('/blog/<string:id>')
-# def blog(id):
-#     items = [item for item in get_blogs() if item.id() == id]
-#     if len(items) == 0:
-#         return home()
-#     return render_template('blog.html', content=markdown2.markdown(items[0].content()), blogs=get_blogs())
+@app.route('/blog/<string:id>')
+def blog(id):
+    items = [item for item in get_blogs() if item.id() == id]
+    if len(items) == 0:
+        return home()
+    return render_template('blog.html', content=markdown2.markdown(items[0].content()), blogs=get_blogs())
 
 
-# @app.route('/rss', methods=['GET'])
-# def rss():
-#     rss_xml = render_template('rss.xml', items=get_blogs())
-#     response = make_response(rss_xml)
-#     response.headers['Content-Type'] = 'application/rss+xml'
-#     return response
+@app.route('/rss', methods=['GET'])
+def rss():
+    rss_xml = render_template('rss.xml', items=get_blogs())
+    response = make_response(rss_xml)
+    response.headers['Content-Type'] = 'application/rss+xml'
+    return response
 
 
 def get_pictures():
--- core/entities.py
+++ core/entities.py
@@ -1,3 +1,32 @@
+import urllib.request
+
+
+class Blog:
+    def __init__(self, title, date, filename) -> None:
+        super().__init__()
+        self.filename = filename
+        self.date = date
+        self.title = title
+
+    def id(self):
+        return self.filename.split('/')[-1].replace('.md', '').strip()
+
+    def content(self):
+        url = "https://raw.githubusercontent.com/earendil06/blog/master/{filename}".format(filename=self.filename)
+        stream = urllib.request.urlopen(url)
+        data = stream.read()
+        stream.close()
+        return data.decode("utf-8")
+
+    def date_rss(self):
+        if self.date == '':
+            return self.date
+        return '{date}T12:00:00+00:00'.format(date=self.date)
+
+    def link(self):
+        return 'https://portfolio.florentpastor.com/blog/' + self.id()
+
+
 class Picture:
     def __init__(self, title, url):
         self.url = url
